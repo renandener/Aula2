62feae5afbee793302c3cdc6edf8b5551fd6ab90
renandener <renan.lacerda@etec.sp.gov.br>
2022-06-11 10:17:37 -0300

    aula2

---
+++ aula_2/lib/main.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('<tchau-tchau>'),
+          ),
+        ),
+      ),
+    );
